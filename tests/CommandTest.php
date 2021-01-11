c151b963422ab604625bebfc2d4d9d02d47170dc
David Mamber <sicsol@streetconcepts.com>
2021-01-11 12:31:22 -0500

    Fixed command test

--- tests/CommandTest.php
+++ tests/CommandTest.php
@@ -7,7 +7,7 @@ class CommandTest extends TestCase
     /** @test */
     public function create_custom_feed()
     {
-        $this->artisan('activitystream:feed TrendingFeed')
+        $this->artisan('make:feed TrendingFeed')
             ->assertExitCode(0);
     }
 }
